0404d01f7d77afa0ed05ba6f92ab660f80110a1c
Codersfirst <>
2019-10-10 19:15:53 +0200

    Big bang

---
+++ src/test/java/com/codersfirst/interview/UnitTestExample.java
@@ -0,0 +1,13 @@
+package com.codersfirst.interview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnitTestExample {
+
+    @Test
+    public void testWork() {
+        assertEquals(2, 2);
+    }
+}

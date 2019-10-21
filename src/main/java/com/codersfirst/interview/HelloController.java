8795d77f6937c895ef39dab1431e5419874b2290
Codersfirst <>
2019-10-22 00:26:47 +0200

    add cors

--- src/main/java/com/codersfirst/interview/HelloController.java
+++ src/main/java/com/codersfirst/interview/HelloController.java
@@ -1,11 +1,15 @@
 package com.codersfirst.interview;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.cors.CorsConfiguration.ALL;
+
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin(origins = ALL)
 public class HelloController {
 
     @GetMapping
